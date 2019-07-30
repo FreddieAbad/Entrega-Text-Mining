08358093a96bc5b8c2325f5774b654cab7409f9f
FreddyMaster <freddy.abadl@ucuenca.edu.ec>
2019-07-30 02:16:28 -0500

    av

---
+++ Trabajo_Final_Freddy_Abad/Resultados/UrlsOut/ConvertirCsvXlsx.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+
+
+
+# In[38]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina1-100.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina1-100.xlsx')
+
+
+# In[39]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina101-200.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina101-200.xlsx')
+
+
+# In[40]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina201-300.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina201-300.xlsx')
+
+
+# In[41]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina301-400.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina301-400.xlsx')
+
+
+# In[42]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina401-500.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina401-500.xlsx')
+
+
+# In[43]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina501-600.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina501-600.xlsx')
+
+
+# In[44]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina601-700.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina601-700.xlsx')
+
+
+# In[45]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina701-800.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina701-800.xlsx')
+
+
+# In[46]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina801-900.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina801-900.xlsx')
+
+
+# In[47]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina901-1000.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina901-1000.xlsx')
+
+
+# In[48]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina1001-1100.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina1001-1100.xlsx')
+
+
+# In[49]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina1101-1200.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina1101-1200.xlsx')
+
+
+# In[50]:
+
+
+from openpyxl import Workbook
+import csv
+
+
+wb = Workbook()
+ws = wb.active
+###
+with open('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\3-CuerpoPagina1200-1300.csv', 'r') as f:
+    for row in csv.reader(f):
+        ws.append(row)
+wb.save('C:\\Users\\Usuario\\Desktop\\Web-Scraping---Text-Mining\\RapidMiner Process\\Entregables\\UrlsOut\\xlsx\\3-CuerpoPagina1200-1300.xlsx')
+
+
+# In[ ]:
+
+
+
+
